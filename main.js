f8304de2e5f5444e88e1fa6726367e0d80118c4c
Triminecraft85 <triminecraft85@gmail.com>
2024-09-17 22:12:15 +0700

    update icon

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import GeoJSON from 'ol/format/GeoJSON.js';
 import VectorLayer from 'ol/layer/Vector.js';
 import VectorSource from 'ol/source/Vector.js';
-import {Map, View} from 'ol';
+import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import {Circle, Fill, Stroke, Style,Text} from 'ol/style';
+import { Icon } from 'ol/style.js';
+import { Circle, Fill, Stroke, Style, Text } from 'ol/style';
 import Overlay from 'ol/Overlay.js';
 import OSM from 'ol/source/OSM';
 import './style.css'
@@ -22,9 +23,50 @@ const sidePanel = document.getElementById('side-panel');
 const sidePanelContent = document.getElementById('side-panel-content');
 const closeBtn = document.getElementById('close-btn');
 
+const geojsonFeature = {
+  "type": "FeatureCollection", "features": [
+    {
+      "id": "11111111-1111-1111-1111-111111111112",
+      "type": "Feature",
+      "feature_type": "anchor",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          11774159.39,
+          1122409.36
+        ]
+      },
+      "properties": {
+        "address_id": "11111111-1111-1111-1111-111111111111",
+        "unit_id": "11111111-1111-1111-1111-111111111111"
+      }
+    }
+  ], "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::404000" } }
+};
+
+
+const vectorSource = new VectorSource({
+  features: new GeoJSON().readFeatures(geojsonFeature, {
+    featureProjection: 'EPSG:3857' // Ensure the coordinates are in the correct projection
+  }),
+});
+
+
+const anchors = new VectorLayer({
+  source: vectorSource,
+  style: new Style({
+    image: new Icon({
+      crossOrigin: 'anonymous',
+      scale: 0.05,
+      src: './eye.svg',
+    }),
+  })
+});
+
+
 
 let roomGEOJSON = new VectorSource({
-  url: 'http://127.0.0.1:8000/api/units',  
+  url: 'http://127.0.0.1:8000/api/units',
   format: new GeoJSON(),
 });
 
@@ -47,38 +89,47 @@ const map = new Map({
       source: new OSM(),
     }),
     room,
+    anchors,
   ],
   target: 'map',
   view: new View({
-    center: [11774143.875, 1122400.1918],
+    center: [11774159.39, 1122409.36],
     zoom: 20,
   }),
 });
 
 //side panel
-closeBtn.onclick = function() {
+closeBtn.onclick = function () {
   sidePanel.style.left = '-300px'; // Hide the side panel
 };
 
-map.on('click', function(evt) {
-  var feature = map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
-    if (layer === room) {
-      return feature;
+map.on('click', function (evt) {
+  map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+    if (layer === room || layer === anchors) {
+
+
+      if (feature) {
+        var panelContent;
+        console.log(feature);
+        if (String(feature.getGeometry().getType()) === "Point") {
+          console.log("thid work");
+          panelContent = '1';
+        }
+        else if (String(feature.getGeometry().getType()) === "Polygon") {
+          panelContent = '<h3>' + feature.get('name').vi + '</h3>';
+          panelContent += '<p>Category: ' + feature.get('category') + '</p>';
+          panelContent += '<p>Level ID: ' + feature.get('level_id') + '</p>';
+          panelContent += '<p>Geometry Type: ' + feature.getGeometry().getType() + '</p>';
+          panelContent += '<p>Coordinates: ' + JSON.stringify(feature.getGeometry().getCoordinates()) + '</p>';
+        }
+
+        sidePanelContent.innerHTML = panelContent;
+        sidePanel.style.left = '0px';
+      } else {
+        sidePanel.style.left = '-300px';
+      }
     }
   });
-
-  if (feature) {
-    var panelContent = '<h3>' + feature.get('name').vi + '</h3>';
-    panelContent += '<p>Category: ' + feature.get('category') + '</p>';
-    panelContent += '<p>Level ID: ' + feature.get('level_id') + '</p>';
-    panelContent += '<p>Geometry Type: ' + feature.getGeometry().getType() + '</p>';
-    panelContent += '<p>Coordinates: ' + JSON.stringify(feature.getGeometry().getCoordinates()) + '</p>';
-
-    sidePanelContent.innerHTML = panelContent;
-    sidePanel.style.left = '0px'; 
-  } else {
-    sidePanel.style.left = '-300px'; 
-  }
 });
 
 
@@ -94,25 +145,27 @@ let highlightStyle = new Style({
 });
 
 let selectedFeature = null;
-map.on('click', function(evt) {
+map.on('click', function (evt) {
   if (selectedFeature) {
     selectedFeature.setStyle(undefined);
     selectedFeature = null;
   }
-  
-  map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+
+  map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+    if (String(feature.getGeometry().getType()) === "Polygon") {
       selectedFeature = feature;
-      feature.setStyle(highlightStyle); 
-      return true; 
+      feature.setStyle(highlightStyle);
+    }
+    return true;
   });
-  
+
 });
 
 //floor change
 function filterRoomsByFloor(floor) {
   let filteredRooms = [];
 
-  roomGEOJSON.forEachFeature(function(feature) {
+  roomGEOJSON.forEachFeature(function (feature) {
     if (String(feature.get('level_id')) === String(floor)) {
       filteredRooms.push(feature);
     }
@@ -122,11 +175,11 @@ function filterRoomsByFloor(floor) {
     features: filteredRooms
   });
 
-  room.setSource(filteredSource);  
+  room.setSource(filteredSource);
 }
 
-document.getElementById('floor-select').addEventListener('change', function(event) {
-  let selectedFloor = String(event.target.value);  
-  filterRoomsByFloor(selectedFloor);  
+document.getElementById('floor-select').addEventListener('change', function (event) {
+  let selectedFloor = String(event.target.value);
+  filterRoomsByFloor(selectedFloor);
 });
 
